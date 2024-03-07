03502d2d87a195c489ce6de4ed21e72145773b6d
남찬현 <chanhyun0821@gmail.com>
2024-03-07 14:41:23 +0900

    feat : board API 목업 추가

---
+++ BE/src/main/java/com/ssafy/coffee/domain/board/dto/BoardGetListResponseDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.coffee.domain.board.dto;
+
+import lombok.Data;
+import java.util.List;
+
+
+@Data
+public class BoardGetListResponseDto {
+    List<BoardGetResponseDto> list;
+    private int totalPages;
+    private long totalElements;
+}
---
+++ BE/src/main/java/com/ssafy/coffee/domain/board/dto/BoardGetResponseDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.coffee.domain.board.dto;
+import com.ssafy.coffee.domain.board.entity.BoardDomain;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class BoardGetResponseDto {
+    private Long boardIndex;
+    private Long userId;
+    private String userName;
+    private List<String> images;
+    private String title;
+    private String content;
+    private BoardDomain domain;
+    private LocalDateTime regDate;
+}
