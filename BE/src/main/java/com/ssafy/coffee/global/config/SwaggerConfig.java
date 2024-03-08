86a7a3c997060311abcc3b0c75e920884882086a
남찬현 <chanhyun0821@gmail.com>
2024-03-08 16:43:02 +0900

    style: swagger 문서화 제목 수정

--- BE/src/main/java/com/ssafy/coffee/global/config/SwaggerConfig.java
+++ BE/src/main/java/com/ssafy/coffee/global/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e38d735be9f254634712f93ce67abb4f282220b61af0f7b3dca1041df278b05
-size 1794
+oid sha256:b0ebbc2aaedd02db6c49b6a46262fe6fee74b129b689fa57d936beaae8fc876d
+size 1798
