810fa970b12722cc93af1aca6c8910c9bcffbc72
강보훈 <kbh10423@gmail.com>
2024-03-11 10:53:39 +0900

    feat : 퐅더 구조 정립

---
+++ fe/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const color1 = Color(0xFFCCD5AE);
+  static const color2 = Color(0xFFE9EDC9);
+  static const color3 = Color(0xFFFEFAE0);
+  static const color4 = Color(0xFFFAEDCD);
+  static const color5 = Color(0xFFD4A373);
+}
