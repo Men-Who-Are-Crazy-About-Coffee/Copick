3efc8f614a8842fe5e9b1d29d041003b4e862bc2
남찬현 <chanhyun0821@gmail.com>
2024-03-11 11:10:58 +0900

    style : 디렉토리 이름 변경

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/member/entity/Member.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70deb839279160b83890162efde602ffd6adb39ba20b555ee7a3d12a268866ae
-size 1484
+oid sha256:af4e3979d7f0d7e27cec9d0a74192688cab37b3cb8299ede127d5407af57789a
+size 1474
