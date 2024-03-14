7b541a311ea7ce2c8b9b416ea5ee0ffe9bfc0904
이효재 <orange1th@gmail.com>
2024-03-14 16:45:16 +0900

    Refactor: Flaw 테이블 의존 관계

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/result/entity/Flaw.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/result/entity/Flaw.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:222098c596b89600e2e0ee3c38122751c6ab80a8c71c147f8917db6f345f3cd9
-size 654
+oid sha256:804c23aa18e1ff437963e7be3dc6a4d2e972f01d9bca03186c4001999820e6c9
+size 648
