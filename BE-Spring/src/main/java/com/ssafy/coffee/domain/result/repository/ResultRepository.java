3936405213af853e69e499e274e27ee948d0f3e4
이효재 <orange1th@gmail.com>
2024-03-14 16:58:04 +0900

    Feat: 빈 Result 생성 API

---
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/result/repository/ResultRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2fd9058633448d41c72b19bd1ee33ef15580e7ebe8a70720c6e27fded329895
+size 480
---
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/result/service/ResultService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff54f93b669061aaf254ca1eb610d76a0620813925335111fd8f4aaf9518f236
+size 1484
