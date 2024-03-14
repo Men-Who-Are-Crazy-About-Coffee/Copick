9fd4a4558bb5b8a96c835e2c4cab8c375431d92a
남찬현 <chanhyun0821@gmail.com>
2024-03-14 16:11:02 +0900

    fix : board repository 검색로직 수정

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/board/repository/BoardRepository.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/board/repository/BoardRepository.java
@@ -1,13 +1,17 @@
 package com.ssafy.coffee.domain.board.repository;
 
 import com.ssafy.coffee.domain.board.entity.Board;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    Page<Board> findByKeywordAndDomain(String keyword, String domain, Pageable pageable);
+    @Query("SELECT b FROM Board b WHERE b.title LIKE %:keyword% AND b.domain = :domain")
+    Page<Board> findByKeywordAndDomain(@Param("keyword") String keyword, @Param("domain") String domain, Pageable pageable);
+
 }
