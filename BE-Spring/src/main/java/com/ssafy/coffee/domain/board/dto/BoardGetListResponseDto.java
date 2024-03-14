c5007fceaa3d4c612418d9fe078cb0be06bd269f
이효재 <orange1th@gmail.com>
2024-03-14 10:28:04 +0900

    Feat: FAST API CORS

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/board/dto/BoardGetListResponseDto.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/board/dto/BoardGetListResponseDto.java
@@ -1,11 +1,3 @@
-package com.ssafy.coffee.domain.board.dto;
-
-import lombok.Data;
-import java.util.List;
-
-@Data
-public class BoardGetListResponseDto {
-    private List<BoardGetResponseDto> list;
-    private int totalPages;
-    private long totalElements;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7afe7e0a738db9a7a991d76f895d65dfb9b0d92bca60dc699c88d43700d3327d
+size 238
--- BE-Spring/src/main/java/com/ssafy/coffee/domain/board/repository/BoardRepository.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/board/repository/BoardRepository.java
@@ -1,10 +1,3 @@
-package com.ssafy.coffee.domain.board.repository;
-
-import com.ssafy.coffee.domain.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7279a8c8bb6c420206bb35a07fd32225e56c18e7b4cf7fbfe53b98c38bd78056
+size 300
