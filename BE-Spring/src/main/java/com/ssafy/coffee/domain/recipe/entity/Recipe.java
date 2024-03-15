62afb84600633dee2dbcaaf53e8f47b7b4f2931f
남찬현 <chanhyun0821@gmail.com>
2024-03-15 13:07:51 +0900

    fix : recipe 엔티티 수정

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/recipe/entity/Recipe.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/recipe/entity/Recipe.java
@@ -4,6 +4,7 @@ import com.ssafy.coffee.domain.bean.entity.Bean;
 import com.ssafy.coffee.domain.roasting.entity.Roasting;
 import com.ssafy.coffee.global.entity.AuditableBaseObject;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -34,5 +35,11 @@ public class Recipe extends AuditableBaseObject {
     @Column(name = "recipe_content", nullable = false, length = 255)
     private String content;
 
-    // Constructors, Setters
+    @Builder
+    public Recipe(Bean bean, Roasting roasting, String title, String content) {
+        this.bean = bean;
+        this.roasting = roasting;
+        this.title = title;
+        this.content = content;
+    }
 }
