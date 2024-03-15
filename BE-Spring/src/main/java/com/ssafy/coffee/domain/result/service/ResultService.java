7cf33aadf7a3238d8aa5cf191fbbdc88f294e9fb
남찬현 <chanhyun0821@gmail.com>
2024-03-15 16:04:55 +0900

    fix : java 버전에 따른List 함수 충돌 수정, 통일

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/result/service/ResultService.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/result/service/ResultService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d293cd5dbb869191989ee2331a79a700810bbff96651476763c473b2135c95d
-size 2376
+oid sha256:c25aa3c48482947eefdd29d6b51475ca59c8d6df08bbb5d119a04598b8c7cd10
+size 2344
