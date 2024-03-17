1736fe740e47d1eb945f26c16a52779a57bda42b
남찬현 <chanhyun0821@gmail.com>
2024-03-17 20:26:14 +0900

    fix : Response data 사용안하도록 수정

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/member/controller/MemberController.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71325d735a330bcabc671db898d7bd95b01eafc7c3d59439734bd9110cbf5105
-size 3546
+oid sha256:8b74650055085ffbd078f20b9bd58d235b99d738cef6569d06491dcc9aa06e47
+size 3239
--- BE-Spring/src/main/java/com/ssafy/coffee/global/response/ResponseData.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:79541be7b7a2c6023c51f492be49b65032d1a0feaf1873acbff2b07838d98a92
-size 744
