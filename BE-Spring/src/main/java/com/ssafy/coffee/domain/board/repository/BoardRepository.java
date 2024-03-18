0e7ec52342cb962e5b3f4ceadf4c31733408d055
남찬현 <chanhyun0821@gmail.com>
2024-03-18 12:17:22 +0900

    fix : rosting api 수정

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/board/repository/BoardRepository.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/board/repository/BoardRepository.java
@@ -1,7 +1,7 @@
 package com.ssafy.coffee.domain.board.repository;
 
 import com.ssafy.coffee.domain.board.entity.Board;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
