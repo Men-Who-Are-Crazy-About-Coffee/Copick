77eec0cee4ccdf5652f106fb9951a3ea7114b53d
이효재 <orange1th@gmail.com>
2024-03-20 15:36:52 +0900

    Refactor: 필요없는 API 삭제

--- BE-Python/server.py
+++ BE-Python/server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbf6a84d7a83b0f05abc08a249b85b5a0b0e2fcebd25830e2488782f6e869432
-size 3822
+oid sha256:b3b8ce45587e2024ba54e871f762112a51c956fd36d6b56bbcc1b59674218957
+size 2549
