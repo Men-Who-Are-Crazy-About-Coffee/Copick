761ff8afb5ea47230ceb2ed735ca6491d54374f2
hoseong <luke1546@naver.com>
2024-03-20 12:50:20 +0900

    feat: test

--- fe/lib/src/screens/login_page.dart
+++ fe/lib/src/screens/login_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8b0b179d8cd1dac7875334b8a49ecc9f762538bd4c95ab4431f8766b1583d2a
-size 5450
+oid sha256:03f7ee82bfa95697a45b03553ed55ad3a2771c481e58486a8ed3b2e1c3d8b731
+size 5431
--- fe/lib/src/services/api_service.dart
+++ fe/lib/src/services/api_service.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26d83581814a48505a20b71f2afb7e7edc6a43e44fe0bc7c1ec348cd4d0d3c9e
-size 2067
+oid sha256:7593b816ad5e04195e95a1de956e7a9f07af01fda5b3db9bd186846f63b72c55
+size 2089
