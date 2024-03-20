e70889b3bbe66b8da21e4440236ccdb393c90131
남찬현 <chanhyun0821@gmail.com>
2024-03-20 13:21:07 +0900

    feat : 권한에 따른 접근설정 적용

--- BE-Spring/src/main/java/com/ssafy/coffee/global/config/SecurityConfig.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.coffee.global.config;
 
+import org.springframework.http.HttpMethod;
 import com.ssafy.coffee.domain.auth.service.CustomOAuth2UserService;
 import com.ssafy.coffee.domain.auth.service.JwtService;
 import com.ssafy.coffee.domain.auth.service.OAuth2LoginFailHandler;
@@ -17,7 +18,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.List;
 
@@ -26,14 +26,14 @@ import java.util.List;
 @EnableWebSecurity //시큐리티 활성화 -> 기본 스프링 필터 체인에 등록
 @RequiredArgsConstructor
 @Slf4j
-public class SecurityConfig  {
+public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final OAuth2LoginFailHandler oAuth2LoginFailHandler;
     private final JwtService jwtService;
     private final CorsConfig corsConfig;
-    private static final String[] PERMIT_PATTERNS=List.of(
+    private static final String[] PERMIT_PATTERNS = List.of(
             "/login"
     ).toArray(String[]::new);
 
@@ -49,19 +49,28 @@ public class SecurityConfig  {
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .csrf((csrf)->csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin((FormLoginConfigurer::disable))
-                .cors((customCorsConfig)->customCorsConfig.configurationSource(corsConfig.corsConfigurationSource()))
+                .cors((customCorsConfig) -> customCorsConfig.configurationSource(corsConfig.corsConfigurationSource()))
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PERMIT_PATTERNS).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll() // 개발시 편하게 하기 위한 설정 반드시 나중에 제거
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+
+                        .requestMatchers(HttpMethod.POST, "/api/auth/register", "/api/auth/refresh", "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/api/auth/logout").hasAnyRole("ADMIN", "USER")
+
+                        .requestMatchers(HttpMethod.POST, "/api/roasting/**", "/api/bean/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/roasting/**", "/api/bean/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/roasting/**", "/api/bean/**").hasRole("ADMIN")
+
+                        .requestMatchers("/api/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
-                .oauth2Login((oauth2)->oauth2
+                .oauth2Login((oauth2) -> oauth2
                         .successHandler(oAuth2LoginSuccessHandler)
                         .failureHandler(oAuth2LoginFailHandler)
-                        .userInfoEndpoint((userInfoEndpoint)->
+                        .userInfoEndpoint((userInfoEndpoint) ->
                                 userInfoEndpoint.userService(customOAuth2UserService))
 
                 )
