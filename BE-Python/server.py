56123beaf96caaf86c9be50a165862f19014ef29
이효재 <orange1th@gmail.com>
2024-03-20 15:36:52 +0900

    Refactor: 필요없는 API 삭제

--- BE-Python/server.py
+++ BE-Python/server.py
@@ -36,48 +36,6 @@ def read_root():
 def test_api():
     return {"test success"}
 
-@app.post("/image")
-async def upload_image(file: UploadFile = File(...)):
-    try:
-        DIR = "./image"
-        if not os.path.exists(DIR): # 해당 디렉토리가 없다면 생성
-            os.makedirs(DIR) 
-        filename = "test2.png"
-        with open(os.path.join(DIR, filename), "wb") as buffer:
-            shutil.copyfileobj(file.file,buffer)
-        return {filename}
-    except:
-        return {"error"}
-    
-@app.post("/s3")
-async def upload_image(files: List[UploadFile] = File(...)):   
-    try:
-        results = []
-        for file in files:
-            s3.upload_fileobj(file.file,os.environ["AWS_S3_BUCKET"],file.filename)
-            results.append("{file.filename} upload success")
-        return {results}
-    except Exception as e:
-        print(e)
-        return {"error"}
-    
-@app.get("/DB")
-async def first_get():
-    try:
-        try:
-            print("income")
-            db_sesson = db_session_maker()
-            response = db_sesson.execute(text("SELECT * FROM member")).fetchall()
-            print(type(response))
-            print(response)
-            print(response[0][0])
-            return response[0][0]
-        finally:
-            db_sesson.close()
-    except Exception as e:
-        print(e)
-        return {"error"}
-    
 @app.post("/api/analyze")
 async def analyze_image(request: Request,
                         resultIndex: str = Form(...), file: UploadFile = File(...)):
