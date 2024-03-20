78db9190fa585c4f92e6f3c3acc52bc6784ff1f9
강보훈 <kbh10423@gmail.com>
2024-03-20 10:50:41 +0900

    feat : logo 변경

--- fe/lib/src/services/api_service.dart
+++ fe/lib/src/services/api_service.dart
@@ -17,11 +17,9 @@ class ApiService {
         .add(InterceptorsWrapper(onRequest: (options, handler) async {
       // 기기에 저장된 AccessToken 로드
       String? accessToken = await storage.read(key: 'ACCESS_TOKEN');
-      if (accessToken != null) {
-        // 매 요청마다 헤더에 AccessToken을 포함
-        options.headers['Authorization'] = 'Bearer $accessToken';
-      }
-
+      // 매 요청마다 헤더에 AccessToken을 포함
+      options.headers['Authorization'] = 'Bearer $accessToken';
+    
       return handler.next(options); // 요청을 계속 진행
     }, onError: (error, handler) {
       // 오류 처리를 위한 코드를 여기에 작성할 수 있습니다.
