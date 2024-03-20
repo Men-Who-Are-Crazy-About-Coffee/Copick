856d2101db7900e55386b29b2c4cfca771797f14
강보훈 <kbh10423@gmail.com>
2024-03-20 10:50:41 +0900

    feat : logo 변경

--- fe/lib/src/services/api_service.dart
+++ fe/lib/src/services/api_service.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b2e30f05c9c20c143084b0cd8e5adab49a39bbe93df4ffc61011e94f680b410
-size 2108
+oid sha256:26d83581814a48505a20b71f2afb7e7edc6a43e44fe0bc7c1ec348cd4d0d3c9e
+size 2067
