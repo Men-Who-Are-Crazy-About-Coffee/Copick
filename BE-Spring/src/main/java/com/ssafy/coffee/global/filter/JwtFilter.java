f3c020ea79974382a49f543b4a501631a050ed5d
mudokja <mudokja@gmail.com>
2024-03-21 14:15:14 +0900

    fix: jwt 버그 해결

--- BE-Spring/src/main/java/com/ssafy/coffee/global/filter/JwtFilter.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/global/filter/JwtFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65d708771eadcae64ca6dd45a809bb00fe6b7e4a8d83d5c678db5b5b97ebbc6f
-size 3633
+oid sha256:52b1952d55c52025e962b200d2b56c0fe7692722f71a47506acbeab346f4b981
+size 3687
