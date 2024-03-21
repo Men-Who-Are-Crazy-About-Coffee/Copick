def27d0a7352c101465490c4800138fcb8bffd8d
mudokja <mudokja@gmail.com>
2024-03-21 16:34:57 +0900

    feat: 탈퇴 회원 삭제 스케줄링

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/member/repository/MemberRepository.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/member/repository/MemberRepository.java
@@ -1,15 +1,3 @@
-package com.ssafy.coffee.domain.member.repository;
-
-import com.ssafy.coffee.domain.member.entity.Member;
-import com.ssafy.coffee.global.constant.AuthType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByIdAndAuthType(String id, AuthType authType);
-    Optional<Member> findById(String memberId);
-    Optional<Member> findByIndexAndIsDeletedFalse(Long index);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4315b5e775bbd0e14547c60addc2a4524288a0e91661861ae78b8a969bbc4144
+size 705
