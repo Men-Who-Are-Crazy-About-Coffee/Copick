72754eb75e0a239fe1fd20b2b1865136205e1fe3
남찬현 <chanhyun0821@gmail.com>
2024-03-21 16:49:16 +0900

    fix : global 엔티티 수정

---
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/global/entity/Global.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fff89db0bd1045f7f292bf18fa31860381bf33e3d15cc62d7290e078d406e52c
+size 388
---
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/global/repository/GlobalRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:be6e8da811959c4086793e9affc9779560de0ee1982b43042d9a7d164af496f9
+size 307
