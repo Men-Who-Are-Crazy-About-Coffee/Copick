16b5859be370256a435c1acef7a1645cd2d8aff1
남찬현 <chanhyun0821@gmail.com>
2024-03-21 09:42:03 +0900

    feat : boardImage 엔티티 생성

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/board/entity/Board.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/board/entity/Board.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4554ee220ac4bbaa78950b82909cea56a92a1ae3fdbfe3b1433800f18f22cb36
-size 1112
+oid sha256:37e9a1c04a13251c3614ce9e52a19ec585143241a7f4e79ab959100919cef32c
+size 1135
