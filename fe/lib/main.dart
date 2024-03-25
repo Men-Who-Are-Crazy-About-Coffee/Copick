9a9ad9595826efc9927b4efd789c9697d6ca8c69
hoseong <luke1546@naver.com>
2024-03-25 13:54:25 +0900

    feat: camera list

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,27 +1,3 @@
-import 'package:camera/camera.dart';
-import 'package:fe/routes.dart';
-import 'package:fe/src/services/camera_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // 사용 가능한 카메라 목록을 가져옵니다.
-  final cameras = await availableCameras();
-  // 첫 번째 카메라를 선택합니다.
-  final firstCamera = cameras.first;
-  await dotenv.load(fileName: ".env");
-
-  return runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-            create: (context) => CameraProvider()..setCamera(firstCamera)),
-      ],
-      child: MaterialApp(
-        routes: routes,
-      ),
-    ),
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9f2358d072c070847842641ae3881d36fd9445658a211772d2a7ebdc4410b40
+size 838
