4f0bc12ae49dcfd0691a38146f7d93156809bcf5
남찬현 <chanhyun0821@gmail.com>
2024-03-25 09:35:57 +0900

    fix : recipe 엔티티 수정

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/recipe/entity/Recipe.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/recipe/entity/Recipe.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:769d17fbb8e588951f52e6211ce7cd50aad68c5036cf189fcef22a912418c0ce
-size 1282
+oid sha256:4d436f5e9392fa5b8453025d2ef7b668050c56542f36348998f3b18b4141b770
+size 1254
