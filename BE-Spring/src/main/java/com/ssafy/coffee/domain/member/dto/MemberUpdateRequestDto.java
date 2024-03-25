5491f152b10db59e6e6b8cd9f01a317130d2159a
남찬현 <chanhyun0821@gmail.com>
2024-03-25 10:48:05 +0900

    fix : member 업데이트 로직 수정

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/member/dto/MemberUpdateRequestDto.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/member/dto/MemberUpdateRequestDto.java
@@ -1,11 +1,3 @@
-package com.ssafy.coffee.domain.member.dto;
-
-import lombok.Data;
-
-@Data
-public class MemberUpdateRequestDto {
-    private String role;
-    private String nickname;
-    private String profileImage;
-    private String password;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c89b93dfb1a6a46adf31abf33edb42036aab1023ceb571b5a24258ff9023d92
+size 230
