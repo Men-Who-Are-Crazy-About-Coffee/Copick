2ac7bcd8470e63cf2889ff2ecabe9f895f40e1ee
강보훈 <kbh10423@gmail.com>
2024-03-26 09:46:09 +0900

    feat : 갤러리 추가 및 build.gradle 수정

--- fe/lib/src/screens/gallery_page.dart
+++ fe/lib/src/screens/gallery_page.dart
@@ -1,74 +1,105 @@
-import 'package:dio/dio.dart';
+import 'package:fe/constants.dart';
+import 'package:fe/src/services/api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:dio/dio.dart';
+import 'package:intl/intl.dart';
 
 class GalleryPage extends StatefulWidget {
   const GalleryPage({super.key});
 
   @override
-  State<GalleryPage> createState() => _GalleryPageState();
+  _GalleryPageState createState() => _GalleryPageState();
 }
 
 class _GalleryPageState extends State<GalleryPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CommentBox(),
-      // Center(
-      //     child: Text(
-      //   "갤러리 페이지입니다.",
-      //   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-      // )),
-    );
-  }
-}
-
-class CommentBox extends StatefulWidget {
-  const CommentBox({super.key});
-
-  @override
-  _CommentBoxState createState() => _CommentBoxState();
-}
-
-class _CommentBoxState extends State<CommentBox> {
-  final String _comment =
-      '이 텍스트는 예시로 사용되는 긴 텍스트입니다. 필요에 따라 내용을 변경하세요. 이 텍스트가 너무 길 경우, 사용자는 전체 내용을 볼 수 있도록 "더보기"를 누를 수 있습니다.';
-  final bool _isExpanded = false;
-  String img =
-      "https://jariyo-s3.s3.ap-northeast-2.amazonaws.com/memeber/anonymous.png";
-
-  final storage = const FlutterSecureStorage();
-  Future<void> isLogin() async {
-    String? accessToekn = await storage.read(key: "ACCESS_TOKEN");
-    if (accessToekn == null) Navigator.pushNamed(context, '/login');
-  }
+  Map<String, List<String>> groupedItems = {}; // 데이터 저장용 상태 변수
 
   @override
   void initState() {
     super.initState();
-    isLogin();
+    getData();
+  }
+
+  Future<void> getData() async {
+    ApiService apiService = ApiService();
+    try {
+      Response response = await apiService.get('/api/result/flaws');
+      // API 응답 처리 및 날짜별로 그룹화
+      Map<String, List<String>> tempGroupedItems = {};
+      for (var item in response.data) {
+        String formattedDate =
+            DateFormat('yyyy-MM-dd').format(DateTime.parse(item['regDate']));
+        tempGroupedItems.putIfAbsent(formattedDate, () => []);
+        tempGroupedItems[formattedDate]!.add(item['image']);
+      }
+
+      setState(() {
+        groupedItems = tempGroupedItems;
+      });
+    } on DioException catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AnimatedCrossFade(
-            firstChild: Text(
-              _comment,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+    ThemeColors themeColors = ThemeColors();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: themeColors.color5,
+        automaticallyImplyLeading: false,
+        title: const Text('지난 원두 분류 사진'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            width: 500,
+            child: Column(
+              children: groupedItems.entries.map(
+                (entry) {
+                  return Column(
+                    children: [
+                      Container(
+                        color: themeColors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(entry.key,
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                              Wrap(
+                                children: entry.value.map((imageUrl) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      child: Image.network(imageUrl,
+                                          width: 70, height: 70),
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                              const Divider(),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      )
+                    ],
+                  );
+                },
+              ).toList(),
             ),
-            secondChild: Text(_comment),
-            crossFadeState: _isExpanded
-                ? CrossFadeState.showSecond
-                : CrossFadeState.showFirst,
-            duration: const Duration(milliseconds: 200),
           ),
-        ],
+        ),
       ),
     );
   }
