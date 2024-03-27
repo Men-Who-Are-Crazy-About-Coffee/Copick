2052669de73a59600bb33b2dc2a92330108a98f2
이효재 <orange1th@gmail.com>
2024-03-27 09:50:35 +0900

    Fix: len 에러

--- BE-Python/functions.py
+++ BE-Python/functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4fa9c18c64b2e555efeaf0b22e04b7c10662c0d8ebd42cfca36e9ea105ad785
-size 7428
+oid sha256:809f930e3b288f489c0d37a73164c95d9bad7a544bd99314ac33c8f4b3f51aeb
+size 7436
