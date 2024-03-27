24cf7bd614b91039cecb27927702e1e9078a79fa
hoseong <luke1546@naver.com>
2024-03-27 16:58:57 +0900

    feat: 게시글 삭제 임시

--- fe/lib/src/screens/profile_page.dart
+++ fe/lib/src/screens/profile_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:214dd3b55dd3dc00d7f3d74a65c403192a6f70b5e7fbb2fd2b3385e9b0d56ea3
+oid sha256:5f86456b8733cf705299a023b58d6225f887b8d0d102390de7fac078f4e6cc51
 size 12946
