a7bc8897a3bce1c9277a17642a568de7b11ca83b
이효재 <orange1th@gmail.com>
2024-03-27 13:46:38 +0900

    Feat: "Flaw 상태 변경 API"

--- BE-Spring/src/main/java/com/ssafy/coffee/domain/result/repository/FlawRepository.java
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/result/repository/FlawRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.coffee.domain.result.repository;
 import com.ssafy.coffee.domain.result.entity.Flaw;
 import com.ssafy.coffee.domain.result.entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,9 @@ import java.util.List;
 @Repository
 public interface FlawRepository extends JpaRepository<Flaw, Long> {
     List<Flaw> findAllByResultIndex(Long resultIndex);
+    List<Flaw> findAllByResultIndexAndIsDeleted(Long resultIndex,boolean isDeleted);
+    @Modifying
+    @Query("update Flaw f set f.isDeleted = true where f.flawIndex in :flawIndexes")
+    void updateAllByIndex(List<Long> flawIndexes);
+
 }
