2189651c35b8776b2d88b8b3059638ea98a93bda
강보훈 <kbh10423@gmail.com>
2024-03-27 16:10:51 +0900

    feat : 갤러리 기능 보완

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,31 +1,3 @@
-import 'package:camera/camera.dart';
-import 'package:fe/constants.dart';
-import 'package:fe/routes.dart';
-import 'package:fe/src/services/camera_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // 사용 가능한 카메라 목록을 가져옵니다.
-  final cameras = await availableCameras();
-  print("cameras : ");
-  print(cameras);
-
-  // 첫 번째 카메라를 선택합니다.
-  final firstCamera = cameras.first;
-  await dotenv.load(fileName: ".env");
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-            create: (context) => CameraProvider()..setCamera(firstCamera)),
-      ],
-      child: MaterialApp(
-        routes: routes,
-      ),
-    ),
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f991e54365e0aabd1d149a99f5151048f5603c85b4f7e779950852a3d5eb4adb
+size 939
