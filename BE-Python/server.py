c61a173d8380b446b61d227cbac1414003ca0433
이효재 <orange1th@gmail.com>
2024-03-28 13:42:54 +0900

    Fix: resultIndext 형식

--- BE-Python/server.py
+++ BE-Python/server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30fb015919f78ca8d64e1e5d31dd0ca574a22afde62c65e3a0c960cf3587692d
-size 4333
+oid sha256:f1bc56a7db1e2284e2b27c006165b3a1ff58ba1d2da1d8b312267308e6c55681
+size 4347
