7759fb7c99fcb24b2ec8a395cf038ac73df7fbf6
hoseong <luke1546@naver.com>
2024-03-28 10:38:42 +0900

    feat: 게시판로딩

--- fe/lib/src/services/board_provider.dart
+++ fe/lib/src/services/board_provider.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80479b0613e2f5f2dd3567de46724433a32f8aff28a7388c43dc16aa515ce273
-size 2756
+oid sha256:831d95759fa24c2b507fc06cc54c9d04a90be0b385146dd99206a3fa4a526c00
+size 2818
