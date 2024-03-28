43d15982142f514368b58b208714b8e1c44a024c
남찬현 <chanhyun0821@gmail.com>
2024-03-28 15:14:10 +0900

    feat : result 페이지 추가

--- fe/lib/src/screens/camera_page.dart
+++ fe/lib/src/screens/camera_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdb06c797e17e7829237c524fbe3a5482f3d90c94f55051278767d853186ea6d
-size 4340
+oid sha256:cde94af725edfd7c48b57490d2c6ddb502cd584b1e971d04afe0f175e09288b0
+size 4597
---
+++ fe/lib/src/screens/result_page.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0688aeefa6eb1d9d1877d3726b46adcfdf7d640814126d81b0b236f1960587d3
+size 3195
