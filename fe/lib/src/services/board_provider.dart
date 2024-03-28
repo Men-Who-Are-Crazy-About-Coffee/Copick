7e3cfc4b82d7190be94aa9345f5a36258e6e4448
hoseong <luke1546@naver.com>
2024-03-28 13:21:57 +0900

    fix: print지움

--- fe/lib/src/services/board_provider.dart
+++ fe/lib/src/services/board_provider.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c8cdc18c12e70fa13f7656da2efe4cd285ab5c5a2e8d59f6696629e71eda5b4
-size 3001
+oid sha256:76d0c498cb2a54d568f651a3daab6b67ca607d4eecae23401238ba1ce8cc4b88
+size 2956
