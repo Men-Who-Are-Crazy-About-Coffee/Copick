a4177060fe8de8b2376db75e7bc253397af0dfcc
mudokja <mudokja@gmail.com>
2024-03-28 14:24:45 +0900

    refactor, feat: 게시글 리팩토링, 좋아요 Redis로 처리 및 스케줄링 동기화,refreshToken 도메인 폴더명 변경
    알려진 문제: 네트워크가 나쁠 때 빠른속도로 좋아요 선택/취소를 반복하면 동기화 문제가 발생할수도 있음

---
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/board/entity/BoardRedisLike.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4737bacff4558fb46f1f7c0a7a9d921446451db9d6531445fb608d471dc65dc8
+size 1095
---
+++ BE-Spring/src/main/java/com/ssafy/coffee/domain/refreshToken/entity/RefreshToken.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8fc65ee10c142a1be6faf91ff6243b555478446eac39209c6a16d3f65110e9c2
+size 1513
