00211ca499e82b4e90c9c9a99bc42464207df8d8
mudokja <mudokja@gmail.com>
2024-03-29 17:50:37 +0900

    feat: 내 게시글 페이지 구현중 임시 저장

--- fe/linux/flutter/generated_plugins.cmake
+++ fe/linux/flutter/generated_plugins.cmake
@@ -1,25 +1,3 @@
-#
-# Generated file, do not edit.
-#
-
-list(APPEND FLUTTER_PLUGIN_LIST
-  file_selector_linux
-  flutter_secure_storage_linux
-)
-
-list(APPEND FLUTTER_FFI_PLUGIN_LIST
-)
-
-set(PLUGIN_BUNDLED_LIBRARIES)
-
-foreach(plugin ${FLUTTER_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/linux plugins/${plugin})
-  target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
-endforeach(plugin)
-
-foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/linux plugins/${ffi_plugin})
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
-endforeach(ffi_plugin)
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f4be2dbcaa3ff895a3dc6fcb9633f0a47c9fdaa7764456aff9459c62d08f3ac
+size 792
