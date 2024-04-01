8d47115cde2623cd8a8c0c3ff93431e1a7ea1f4e
hoseong <luke1546@naver.com>
2024-04-01 10:19:24 +0900

    fix: comment length fix

--- fe/lib/src/widgets/board_container.dart
+++ fe/lib/src/widgets/board_container.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52a398e3303d20d8fd58a3472bab31b2ceb6a24e7b454afe530a7c9ebeee1f59
+oid sha256:c0ade72c8af7827d2984f9c9277a5645f783143f790e57d0d1659ffa77df3cb7
 size 13414
