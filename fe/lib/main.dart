8be18ab0f808ca5cf410310aba8fcc64e8140a2c
강보훈 <kbh10423@gmail.com>
2024-04-02 21:32:12 +0900

    fix : 달력 한글화 및 테마 수정

--- fe/lib/main.dart
+++ fe/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98be769fa4a544787ef19f805e8a3f5679a69fb18cde54e0ef8381afd4889b8e
-size 1820
+oid sha256:f9e44b0fb4ecbf4e4a299fd3ceafb5582b39f9593d76433e67e12068b01d9c95
+size 2115
