65f3d7d552194581aac47adbd18c3a66df06eb3a
hoseong <luke1546@naver.com>
2024-04-02 17:11:18 +0900

    fix: my content 테마색, 뒤로가기, 정렬버튼 수정git add .

--- fe/lib/src/screens/my_content_page.dart
+++ fe/lib/src/screens/my_content_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7daec8953bffa6c6c4886c11dc5516c24e87919844c4ab790f12633328f1a7d1
-size 5278
+oid sha256:a98115909699a1ecac4576f98e5494475e814c2cefdaa82b8bb2e963c3a13087
+size 5593
--- fe/lib/src/widgets/comment_container.dart
+++ fe/lib/src/widgets/comment_container.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4dcb00e13e00dd9d0b4d7cd90fad4484600055ca3bb157b81ca5a8a863ef78a
-size 6729
+oid sha256:515e35e8c8e5dc22bf84185f6a2f97ba5e12d562962b4fb51606a9c65e3a33c9
+size 6628
