6b549301d8110ae78f179f5158f798c72e19f1f9
강보훈 <kbh10423@gmail.com>
2024-04-02 15:17:06 +0900

    fix : 크기 조정

--- fe/lib/src/screens/result_page.dart
+++ fe/lib/src/screens/result_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:245c521cbb4a6602ce2bede4a16edd9995fc140e8eb02ed52c8258c9db7ff802
+oid sha256:d92f3b82928e08bf739dc5849acf0386a55d8945007d4677df9502809ae5bf33
 size 7525
