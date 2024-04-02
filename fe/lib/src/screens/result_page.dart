d5950f5ee3284d5ee5b16f6371a7d2025bd95c9f
hoseong <luke1546@naver.com>
2024-04-02 17:32:28 +0900

    fix: homebutton

--- fe/lib/src/screens/result_page.dart
+++ fe/lib/src/screens/result_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d92f3b82928e08bf739dc5849acf0386a55d8945007d4677df9502809ae5bf33
-size 7525
+oid sha256:22af43b8487850480bd924607e4f8e19562918844ddba29f75a9fb9e2c729ded
+size 7572
