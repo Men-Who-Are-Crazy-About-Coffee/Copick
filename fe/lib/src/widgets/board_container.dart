d7bd314fba5bdf5969ac800cbbb02bf123ec36aa
hoseong <luke1546@naver.com>
2024-04-02 11:00:28 +0900

    fix: image 버그수정 => image_compress

--- fe/lib/src/widgets/board_container.dart
+++ fe/lib/src/widgets/board_container.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ade72c8af7827d2984f9c9277a5645f783143f790e57d0d1659ffa77df3cb7
-size 13414
+oid sha256:93ba15b5337a7516557d7c0f434e2523ee129c4eff9735fe65daa093d764f4c6
+size 13472
