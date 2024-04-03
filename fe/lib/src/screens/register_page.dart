5286787de1cae5c2418d5b4b8d7c0a1311f5a2ec
hoseong <luke1546@naver.com>
2024-04-03 10:43:03 +0900

    feat: 비밀번호 유효성 규칙 추가

--- fe/lib/src/screens/register_page.dart
+++ fe/lib/src/screens/register_page.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3bc5e9ffcf6c3fe7ad6f929073b62533eccb56d326593613bcbfe94748c359e
-size 4889
+oid sha256:41d6811ae0e15f8a25bc4da0c786ea2ef22df228e119311a61114382399c7bbd
+size 6059
