97792356a5e6cc98b1230617877bb1e0e7dfd3f3
남찬현 <chanhyun0821@gmail.com>
2024-04-03 17:38:16 +0900

    fix : detect 세팅 수정

--- BE-Python/functions.py
+++ BE-Python/functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:793b92615f25351d73e9485d38f96295dee6875b4c2ed25ce471d62529a9773e
-size 7618
+oid sha256:b1f07b78bd2e5674fcd9c91aa7df60ec9f6f1f9a3763d0ad9d6bb906df21bf89
+size 7583
